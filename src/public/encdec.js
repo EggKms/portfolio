d19b2d486867e4b9eceda11772169109023a93dd
eggkms <eggrlaalstjr@gmail.com>
2025-06-17 23:00:41 +0900

    fix, encdnc

--- src/public/encdec.js
+++ src/public/encdec.js
@@ -1,16 +1,9 @@
 async function hashData(data) {
   const encoder = new TextEncoder();
-  const encodedData = encoder.encode(data);
-
-  // SHA-256 해시 생성
-  const hashBuffer = await crypto.subtle.digest('SHA-256', encodedData);
-
-  // ArrayBuffer를 16진수 문자열로 변환
+  const dataBuffer = encoder.encode(data);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join('');
-
-  return hashHex;
+  return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
 }
 
-
-export { hashData};
+export { hashData };
